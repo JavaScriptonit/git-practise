8b964c53e0ae7c2f7af122ebf16a2c08cd5ae90d
Andrey Shabunov <shabunovaa@yandex.ru>
2025-03-15 23:42:07 +0300

    Initial commit

--- git_demo.sh
+++ git_demo.sh
@@ -13,40 +13,48 @@ git checkout -B master
 git add .
 git commit -m "Initial commit"
 
-# Создание веток с коммитами
+# Создание ветки feature/1 с 5 коммитами
 echo "Creating feature/1 branch..."
 git checkout -b feature/1
-echo "Feature 1 implementation" > feature1.txt
-git add feature1.txt
-git commit -m "Add feature1.txt in feature/1"
+for i in {1..5}; do
+  echo "Feature 1 commit $i" > feature1_$i.txt
+  git add feature1_$i.txt
+  git commit -m "Feature 1 commit $i"
+done
 
+# Создание ветки feature/2 с 5 коммитами
 echo "Creating feature/2 branch..."
 git checkout master
 git checkout -b feature/2
-echo "Feature 2 implementation" > feature2.txt
-git add feature2.txt
-git commit -m "Add feature2.txt in feature/2"
+for i in {1..5}; do
+  echo "Feature 2 commit $i" > feature2_$i.txt
+  git add feature2_$i.txt
+  git commit -m "Feature 2 commit $i"
+done
 
-# Слияние с использованием Git Merge
+# Слияние feature/1 в master
 echo "Merging feature/1 into master using git merge..."
 git checkout master
 git merge feature/1 -m "Merge feature/1 into master"
 
-# Создание ветки bugfix/1
+# Создание bugfix/1 с 5 коммитами
 echo "Creating bugfix/1 branch..."
 git checkout -b bugfix/1
-echo "Bug fix implementation" > bugfix1.txt
-git add bugfix1.txt
-git commit -m "Add bugfix1.txt in bugfix/1"
+for i in {1..5}; do
+  echo "Bug fix commit $i" > bugfix1_$i.txt
+  git add bugfix1_$i.txt
+  git commit -m "Bug fix commit $i"
+done
 
-# Слияние с использованием Git Rebase
+# Возвращаемся к master и выполняем merge bugfix/1
+echo "Merging bugfix/1 into master using git merge..."
+git checkout master
+git merge bugfix/1 -m "Merge bugfix/1 into master"
+
+# Возвращение к feature/2 для выполнения rebase
 echo "Rebasing feature/2 onto master..."
 git checkout feature/2
 git rebase master
-
-echo "Merging feature/2 into master using git merge..."
-git checkout master
-git merge feature/2 -m "Merge feature/2 into master"
 
 # Итоговое состояние репозитория
 echo "Final state of the repository:"
