ae058527bdae92733f747d198a6819c7c515cbcb
Andrey Shabunov <shabunovaa@yandex.ru>
2025-03-16 22:33:39 +0300

    Bug fix commit 51

--- rebase_merge-no-ff_demo.sh
+++ rebase_merge-no-ff_demo.sh
@@ -4,7 +4,7 @@
 echo "Creating bugfix/1 branch..."
 git checkout -b bugfix/1
 git checkout bugfix/1
-for i in {28..29}; do
+for i in {51..53}; do
   cd /Users/andreyshabunov/PhpstormProjects/git-practics/bugfix1/
   echo "Bug fix commit $i" > bugfix1_$i.txt
   cd /Users/andreyshabunov/PhpstormProjects/git-practics/
