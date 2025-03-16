c7358cf73d9ce5a2e3ca302ef9af18d9c1ba9cd5
Andrey Shabunov <shabunovaa@yandex.ru>
2025-03-16 22:19:46 +0300

    Bug fix commit 34

---
+++ line_merge_squash_demo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Создание bugfix/1 с 5 коммитами
+echo "Creating bugfix/1 branch..."
+git checkout -b bugfix/1
+git checkout bugfix/1
+for i in {34..36}; do
+  cd /Users/andreyshabunov/PhpstormProjects/git-practics/bugfix1/
+  echo "Bug fix commit $i" > bugfix1_$i.txt
+  cd /Users/andreyshabunov/PhpstormProjects/git-practics/
+  git add .
+  git commit -m "Bug fix commit $i"
+  git push -u origin bugfix/1
+done
+
+# Возвращаемся к main и обновляем его
+echo "Updating main branch..."
+git checkout main
+git pull origin main
+
+# Выполняем squash коммитов из bugfix/1
+echo "Squashing commits from bugfix/1..."
+git checkout bugfix/1
+git reset --soft $(git merge-base main bugfix/1)  # Перемещаем указатель на общий предок, сохраняя изменения в индексе
+git commit -m "Squashed bugfix/1 commits"  # Создаём один коммит из всех изменений
+
+# Возвращаемся к main и выполняем merge bugfix/1
+echo "Merging bugfix/1 into main using git merge..."
+git checkout main
+git merge --no-ff bugfix/1 -m "Merge bugfix/1 into main"
+git push -u origin main
+
+# Итоговое состояние репозитория
+echo "Final state of the repository:"
+git log --oneline --graph --all | less
